07936bdd4becb959c0c7bfed7254dbb873c314a0
Aurel <aurelien.delory@grenoble-inp.org>
2022-10-30 10:39:56 +0100

    5

--- approximate_pi.py
+++ approximate_pi.py
@@ -1,42 +1,48 @@
 #! /usr/bin/env python3
-"""Module d'approximation de pi du projet BPI"""
+"""pi approximation module"""
 from random import uniform
-from sys import argv, exit
+from sys import argv
 
 
-def point_aleatoire():
-    """Génère et retourne les coordonnées d'un point (x,y)
-    avec x appartenant à [-1,1] et y appartenant à [-1,1]
-    sous la forme d'un couple"""
+def random_point():
+    """
+    Generates and returns a point (x,y) with -1<=x,y<=1
+    x and y are floating numbers
+    """
     return (uniform(-1, 1), uniform(-1, 1))
 
 
-def appartient_cercle(point):
-    """Retourne vrai si le point se situe dans le disque unitaire.
-    point est un couple (x,y) avec x appartenant à [-1,1] et y appartenant à [-1,1]"""
+def in_circle(point):
+    """
+    Returns True if the point is in the disc of radius 1 and False otherwise
+    """
     return (point[0]**2+point[1]**2) <= 1
 
 
-def approximation_pi(n):
-    """Approximation de pi à l'aide de la méthode de Monte-Carlo en utilisant n points"""
-    compteur = 0
+def pi_approximate(n):
+    """
+    Returns an approximation of pi using the Monte-Carlo method with n points
+    """
+    points_in_circle = 0
     for _ in range(n):
-        compteur += int(appartient_cercle(point_aleatoire()))
-    # compteur/n correspond à une approximation de l'aire du disque divisé par l'aire du carré,
-    # soit pi/4 ici
-    return 4*compteur/n
+        points_in_circle += int(in_circle(random_point()))
+    # points_in_circle/n corresponds to an approximation of
+    # the area of the disc divided by the area of the square,
+    # which is pi/4
+    return 4*points_in_circle/n
 
 
 def main():
-    """Renvoie une approximation de pi à l'aide de la méthode de Monte-Carlo
-    en utilisant le nombre de points indiqués en argument"""
+    """Returns an approximation of pi using the Monte-Carlo method
+    using the number passed in as an argument
+    """
     if len(argv) != 2:
         print(
-            "usage: approximate_pi.py n "
+            "use: approximate_pi.py n "
         )
         exit(1)
     else:
-        print(approximation_pi(int(argv[1])))
+        print(pi_approximate(int(argv[1])))
 
 
 if __name__ == "__main__":
--- draw.py
+++ draw.py
@@ -1,40 +1,116 @@
 #! /usr/bin/env python3
-import approximate_pi
 """Module graphique du projet BPI"""
+import subprocess
+import approximate_pi
 
 
-def genere_liste_points(n, taille):
-    """Retourne une liste composée de ⌊n/10⌋ points(x,y)
-    avec x appartenant à [-⌊taille/2⌋,⌊taille/2⌋] et y appartenant à [-⌊taille/2⌋,⌊taille/2⌋]
-    x et y sont entiers
-    Retourne aussi l'approximation de pi obtenue à partir de ces points"""
-    liste_points = []
-    compteur = 0  # compte les points dans le cercle
-    for _ in range(int(n/10)):
-        point = approximate_pi.point_aleatoire()
-        compteur += int(approximate_pi.appartient_cercle(point))
-        point = (round(point[0]*taille/2), round(point[1]*taille/2))
-        liste_points.append(point)
-    return liste_points, 4*compteur/int(n/10)
-
-
-# ne pas passer par une liste intermédiaire: directement stocker les points dans la string utilisée par le fichier ppm
-
-def generate_ppm_file(taille, n, nb_chiffre):
+def generate_pixel_dict(size):
     """
-    Génère un fichier ppm de fond blanc
-    taille>=100
-    n>=100
-    1<=nb_chiffre<=5
-    Les 3 paramètres sont des entiers"""
-    liste_pi = []  # contient les valeurs estimées de pi
+    Returns a dictionnary with keys from 0 to size²-1
+    representing pixels. Each key is associated with
+    (1,1,1), a white pixel
+    """
+    pixel_dict = {}
+    for i in range(size**2):
+        pixel_dict[i] = (1, 1, 1)
+    return pixel_dict
+
+
+def coordinate_conversion(point, size):
+    """
+    Transforms a point of coordinate (x,y)
+    in an other point of coordinate (u,v) and returns it
+    -1<x,y<1
+    0<=u,v<=size-1
+    x and y are floating numbers
+    u and v are integers
+    """
+    u = int((point[0]+1)*size/2)
+    v = int((point[1]+1)*size/2)
+    if u == size:
+        u -= 1
+    if v == size:
+        v -= 1
+    return (u, v)
+
+
+def next_step(pixel_dict, size, n):
+    """
+    Generates ⌈n/10⌉ points, stores them as pixels in
+    pixel_dict. They are stored as (1,0,0) (red) if they are
+    in the disc and (0,0,1) (blue) otherwise. Also returns the
+    approximation of pi calculated with these points
+    """
+    in_circle_points = 0
+    for _ in range(int(n/10 + 1)):
+        point = approximate_pi.random_point()
+        in_circle = approximate_pi.in_circle(point)
+        point = coordinate_conversion(point, size)
+        if in_circle:
+            in_circle_points += 1
+            pixel_dict[point[1]*size+point[0]] = (1, 0, 0)
+        else:
+            pixel_dict[point[1]*size+point[0]] = (0, 0, 1)
+    return 4*in_circle_points/int(n/10 + 1)
+
+
+def generate_ppm_file(i, size, displayed_pi, pixel_dict):
+    """
+    Generates a ppm file of dimension size*size,
+    maximum color value of 1, color informations are in binary.
+    The pixels are stored in pixel_dict.
+    Returns the file's name which contains the number of the step as well as
+    the current value of the approximation of pi.
+    """
+    img_name = f"img{i}_"+str(displayed_pi).replace(".", "-")+".ppm"
+    with open(img_name, "w", encoding="UTF-8") as img:
+        img.write(f"P6\n{size} {size}\n1\n")
+    with open(img_name, "ab") as img:
+        for i in range(size**2):
+            pixel_string = "%c%c%c" % (
+                pixel_dict[i][0], pixel_dict[i][1], pixel_dict[i][2])
+            img.write(pixel_string.encode("utf-8"))
+    return img_name
+
+
+def average(number_list):
+    """Return the average of the numbers in the list"""
+    return sum(number_list)/len(number_list)
+
+
+def modify_digit_number(number, digit_number):
+    """
+        Return a new number which is the number
+        passed in with only digit_number digits
+        after its comma
+    """
+    return int(number*10**digit_number)/10**digit_number
+
+
+def main(size, n, digit_number):
+    """
+    Generates the 10 ppm files and the gif
+    size,n>10
+    1<=digit_number<=5
+    size, n and digit_number are integers
+    """
+    pi_list = []  # contains the 10 approximations of pi
+    file_names = []
+    pixel_dict = generate_pixel_dict(size)
     for i in range(10):
-        liste_points, approx_pi = genere_liste_points(n, taille)
-        liste_pi.append(approx_pi)
-        # on ne garde que le nombre de chiffres voulu
-        approx_pi = int((approx_pi)e(nb_chiffre))e(-nb_chiffre)
-        with open(f"img{i}_{int(approx_pi)-{approx_pi-int(approx_pi)}}", "w", encoding="UTF-8") as img:
-            img.write(f"P3\n{taille} {taille}\n1")
+        pi_approx = next_step(pixel_dict, size, n)
+        pi_list.append(pi_approx)
+        displayed_pi = average(pi_list)
+        displayed_pi = modify_digit_number(displayed_pi, digit_number)
+        file_names.append(generate_ppm_file(i, size, displayed_pi, pixel_dict))
+        convert(file_names)
 
 
-print(genere_liste_points(100000, 6))
+def convert(file_names):
+    """The images name contained in file_name
+    are used to generate a gif of name pi_approximate.gif"""
+    cmd = ['convert'] + file_names + ['pi_approximate.gif']
+    subprocess.call(cmd)
+
+
+main(1000, 100000, 5)
